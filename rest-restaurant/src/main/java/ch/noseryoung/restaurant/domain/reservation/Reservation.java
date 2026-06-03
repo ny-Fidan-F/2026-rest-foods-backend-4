36dc0247cde19f9336063cef23ca827beb440b69
ny-fabian-b <fabian.blatter@noseryoung.ch>
2026-06-03 13:42:41 +0200

    update Reservation class for repository

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/reservation/Reservation.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/reservation/Reservation.java
@@ -4,32 +4,44 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@jakarta.persistence.Table(name = "reservation")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    UUID id;
+    private UUID id;
 
-    @NotNull(message = "Start date cannot be blank")
-    LocalDateTime start;
+    @NotNull(message = "Start date cannot be null")
+    @Column(name = "start_time")
+    private LocalDateTime start;
 
-    @NotNull(message = "End date cannot be blank")
-    LocalDateTime end;
+    @NotNull(message = "End date cannot be empty")
+    @Column(name = "end_time")
+    private LocalDateTime end;
 
     @Positive(message = "Number of people cannot be zero")
-    int numberOfPeople;
+    private int numberOfPeople;
 
     @NotBlank(message = "Reservee last name cannot be blank")
-    String reserveeLastName;
+    private String reserveeLastName;
 
-    @NotBlank(message = "Reservee last name cannot be blank")
-    String reserveePhoneNumber;
+    @NotBlank(message = "Reservee phone number cannot be blank")
+    private String reserveePhoneNumber;
 
     @ManyToOne
     /* We have to use JoinColumn because it is an entity relationship */
     @JoinColumn(name = "reservation_table")
-    Table table;
+    private RestaurantTable table;
 }
