f1c9602ac034e1d3db1198318e459396d154f9fd
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-06-03 11:29:30 +0200

    Add name field to entity

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/Menu.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/Menu.java
@@ -1,6 +1,7 @@
 package ch.noseryoung.restaurant.domain.menu;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,4 +21,10 @@ public class Menu {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "menu_id")
     private UUID menuId;
+
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+
 }
