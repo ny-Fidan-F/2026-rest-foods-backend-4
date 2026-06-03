9700b22255f317887d2c61cf21a4daa4fc95dcef
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-06-03 15:40:45 +0200

    Add logging to MenuService

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/MenuService.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/MenuService.java
@@ -1,5 +1,7 @@
 package ch.noseryoung.restaurant.domain.menu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +11,29 @@ import java.util.UUID;
 @Service
 public class MenuService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MenuService.class);
+
     @Autowired
     private MenuRepository menuRepository;
 
     public Menu createMenu(Menu menu) {
+        logger.info("Creating menu");
         return menuRepository.save(menu);
     }
 
     public Menu getMenuById(UUID id) {
+        logger.info("Getting menu with id: {}", id);
         return menuRepository.findById(id)
                 .orElseThrow();
     }
 
     public List<Menu> getAllMenus() {
+        logger.info("Getting all menus");
         return menuRepository.findAll();
     }
 
     public Menu updateMenu(UUID id, Menu updatedMenu) {
+        logger.info("Updating menu with id: {}", id);
         Menu existingMenu = getMenuById(id);
 
         existingMenu.setName(updatedMenu.getName());
@@ -39,6 +47,7 @@ public class MenuService {
     }
 
     public void deleteMenu(UUID id) {
+        logger.info("Deleting menu with id: {}", id);
         Menu existingMenu = getMenuById(id);
         menuRepository.delete(existingMenu);
     }
