8bbfefdbcdee75d4fab939932393c7fe05e9892e
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-06-03 11:12:35 +0200

    Add JPA entity annotations and ID field

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/Menu.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/Menu.java
@@ -1,4 +1,23 @@
 package ch.noseryoung.restaurant.domain.menu;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "menus")
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "menu_id")
+    private UUID menuId;
 }
