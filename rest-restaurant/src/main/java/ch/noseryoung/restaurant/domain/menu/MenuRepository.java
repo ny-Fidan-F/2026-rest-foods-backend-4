ff503ab62fce232b0043319611e916d0fa379cae
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-06-03 11:46:04 +0200

    Add JpaRepository implementation

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/MenuRepository.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/menu/MenuRepository.java
@@ -1,4 +1,10 @@
 package ch.noseryoung.restaurant.domain.menu;
 
-public interface MenuRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
 }
