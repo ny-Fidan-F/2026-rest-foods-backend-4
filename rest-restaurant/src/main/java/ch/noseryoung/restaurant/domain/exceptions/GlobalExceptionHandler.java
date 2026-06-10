5cafdca2d958125fdfbf15e9d24c6cd040f0f76a
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-06-10 16:51:15 +0200

    handle validation errors in global exception handler

--- rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/exceptions/GlobalExceptionHandler.java
+++ rest-restaurant/src/main/java/ch/noseryoung/restaurant/domain/exceptions/GlobalExceptionHandler.java
@@ -2,11 +2,26 @@ package ch.noseryoung.restaurant.domain.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        String message = exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception) {
